dfc79c6b0996cc91d2733b0054e5ad765bf081d4
sangramaltur <sangramaltur@gmail.com>
2019-05-26 06:26:01 +0000

    The Flask Mega-Tutorial Part I: Hello, World!

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/vagrant/microblog/venv"
+set -gx VIRTUAL_ENV "/home/vagrant/Blog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
